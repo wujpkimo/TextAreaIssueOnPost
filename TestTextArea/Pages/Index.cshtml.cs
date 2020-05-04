cfc7c100f205a1900685754dc7936ae2aac19089
Jason <wujpkimo@gmail.com>
2020-05-04 17:09:50 +0800

    加入專案檔案。

---
+++ TestTextArea/Pages/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace TestTextArea.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        [BindProperty, MaxLength(300)]
+        public string MainText { get; set; }
+
+        public void OnGet()
+        {
+            MainText = "GetTest";
+        }
+
+        public void OnPost()
+        {
+            MainText = "PostTest";
+        }
+    }
+}
